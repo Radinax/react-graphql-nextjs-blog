04eca87e8b236d49997bdf04ad63a10b13b6d02a
Adrian <adrberia@gmail.com>
2021-12-21 17:52:41 -0400

    Fixed Post Detail name and calendar position

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -31,7 +31,7 @@ const PostDetail = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg lg:p-8 p-4 mb-8 border">
       <div className="px-4 lg:px-0">
-        <div className="flex lg:items-center items-start mb-4 w-full flex-col">
+        <div className="flex lg:items-center items-start mb-4 w-full lg:flex-row flex-col">
           <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
             <Image
               alt={post.author.name}
@@ -45,7 +45,7 @@ const PostDetail = ({ post }) => {
               {post.author.name}
             </p>
           </div>
-          <div className="font-medium text-gray-700 w-60 lg:mb-4">
+          <div className="font-medium text-gray-700 w-60">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 inline mr-2 text-pink-500"
--- components/Tag.js
+++ components/Tag.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Tag = ({ children, onClick }) => {
+const Tag = ({ children, onClick, ...props }) => {
   return (
     <div
       onClick={onClick}
+      {...props}
       className="transition duration-500 ease hover:bg-white inline-block bg-gray-quote rounded-full px-8 py-1 cursor-pointer mb-3 mr-3 font-bold"
     >
       {children}
