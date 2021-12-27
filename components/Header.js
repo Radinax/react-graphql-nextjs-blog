390d84fb2a6d368054f7c5e6b9089ce6d1f6b51b
Adrian <adrberia@gmail.com>
2021-12-27 13:43:44 -0400

    Fixed MD tables on mobile and centered title on mobile

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto lg:px-10 px-2 mb-8">
       <div className="border-b w-full inline-block border-black-400 py-8">
-        <div className="md:float-left block">
+        <div className="md:float-left block lg:text-left text-center">
           <Link href="/">
             <span className="cursor-pointer font-bold text-2xl md:text-4xl">
               Adrian Programming Blog
